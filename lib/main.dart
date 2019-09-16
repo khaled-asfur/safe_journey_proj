b224eb8ea726051988595b65e9371978f18ff7fa
khaled-test <55381412+khaled-test@users.noreply.github.com>
2019-09-16 11:48:02 +0300

    test2
    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:safe_journey/pages/login_page.dart';
 import 'pages/home_page.dart';
 import 'pages/signup_page.dart';
-/** looooooooooooooooool */
+/** loooeeeeeeeeeeeeekkkkkooooooool */
 
 void main() => runApp(MyApp());
 
